34a875caefda9d2851a2474bd478dbcd789e9586
Aditya Sharma <aditya.sharma9301@gmail.com>
2021-04-13 19:52:48 +0530

    completed working model

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -4,57 +4,39 @@ const minutes_el = document.getElementById('minutes_display');
 const seconds_el = document.getElementById('seconds_display'); 
 
 const getDate =  document.getElementById('dateInput').value;
-const date_button = document.getElementById('datebtn');
 
-date_button.click(function() {
-    
-});
-
-console.log(getDate);
-
-const date_input = Date.parse(getDate);
 
 function main() {
 
+    var raw_date =  document.getElementById('dateInput').value;
+    date_input = Date.parse(raw_date);
+
+    document.getElementById('dateInput').addEventListener("change" , function() {
+        var input = this.value;
+        var dateEntered = new Date(input);
+
+        date_input = Date.parse(dateEntered);
+        console.log(date_input);
+    });
+
+    // var display_date = document.getElementById('dateInput').value;
+    // console.log(getElementById(display_date));
+
     const time_now = new Date().getTime();
-    const time_diff =Math.round(Math.floor(date_input - time_now)/1000);
+    const time_diff = Math.round(Math.floor(date_input - time_now)/1000);
 
     var total_time = time_diff;
-    // console.log(total_time);
 
     const days = Math.floor(total_time / 3600/24);
     const hours = Math.floor(total_time/3600 % 24);
     const minutes = Math.floor(total_time / 60 % 60);    
     const seconds = Math.floor(total_time % 60);
 
-    // console.log(days , hours , minutes , seconds);
-
-
-    // function convert_time(total_time) {
-    //     const days = Math.floor(total_time / 3600/24);
-    //     const hours = Math.floor(total_time/3600 % 24);
-    //     const minutes = Math.floor(total_time / 60 % 60);    
-    //     const seconds = Math.floor(total_time % 60);
-
-    //     console.log(days , hours , minutes , seconds);
-    // }
-
-    // function countDown() {
-    //     const newYearsDate = new Date(newYears);
-    //     const currentDate = new Date();
-        
-    //     const total_seconds = (newYearsDate - currentDate)/1000;
-
-    //     const days = Math.floor(total_seconds / 3600/24);
-    //     const hours = Math.floor(total_seconds/3600 % 24);
-    //     const minutes = Math.floor(total_seconds / 60 % 60);    
-    //     const seconds = Math.floor(total_seconds % 60);
-
-        days_el.innerText = days;
-        hours_el.innerText = hours;
-        minutes_el.innerText = minutes;
-        seconds_el.innerText = seconds;
-    // }
+    days_el.innerText = days;
+    hours_el.innerText = hours;
+    minutes_el.innerText = minutes;
+    seconds_el.innerText = seconds;
+    
 }
-// countDown();
+
 setInterval(main , 1000);
